ae6c3cd9fbd97dfab23575c97ee7a7ddc28adf26
Jesus Ayquipa <jesusayquipa789@gmail.com>
2025-06-12 21:29:05 -0500

    Datos Añadidos de la Base de datos

--- src/Conexion/BaseDeDatos.sql
+++ src/Conexion/BaseDeDatos.sql
@@ -35,5 +35,19 @@ VALUES
 ('Empleado'),
 ('Secretario');
 
-SELECT FROM Persona
+INSERT INTO Persona (Nombre, Apellido, Correo, Fecha_Nacimientro, Pais, rol_id, Profesion)
+VALUES
+('Carlos', 'Lopez', 'carlos.lopez@gmail.com', '1990-02-15', 'España', 1, 'Abogado'),
+('Ana', 'Gomez', 'ana.gomez@yahoo.com', '1985-08-23', 'Peru', 2, 'Ingeniero'),
+('Juan', 'Perez', 'juan.perez@gmail.com', '1993-05-12', 'México', 3, 'Contador'),
+('Maria', 'Rodriguez', 'maria.rodriguez@hotmail.com', '1990-07-10', 'Argentina', 4, 'Secretaria'),
+('Pedro', 'Martinez', 'pedro.martinez@outlook.com', '1988-09-17', 'Chile', 1, 'Gerente'),
+('Lucia', 'Fernandez', 'lucia.fernandez@aol.com', '1992-03-25', 'España', 2, 'Arquitecta'),
+('Luis', 'Gonzalez', 'luis.gonzalez@icloud.com', '1987-11-30', 'Colombia', 3, 'Programador'),
+('Laura', 'Lopez', 'laura.lopez@live.com', '1995-01-20', 'Peru', 4, 'Diseñadora'),
+('Ricardo', 'Sanchez', 'ricardo.sanchez@yahoo.com', '1990-04-11', 'México', 1, 'Abogado'),
+('Elena', 'Diaz', 'elena.diaz@gmail.com', '1985-12-02', 'Argentina', 2, 'Ingeniera');
+
+
+
 
